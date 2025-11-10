1c66f91e572ee00ef1ae56dc2656ceee3cb7a0b2
B-Lamy <brendonlamy@gmail.com>
2025-11-09 20:13:31 -0500

    test brendon

--- FileClient/src/main/java/ca/concordia/Main.java
+++ FileClient/src/main/java/ca/concordia/Main.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+//
+// COEN346
 public class Main {
     public static void main(String[] args) {
         //Socket CLient
